b0a9b3e063f969ce920200d1414794091be475f8
jinfagang <nicholasjela@gmail.com>
2017-02-25 12:28:06 +0800

    add file reader and writer

---
+++ qing_file_reader.h
@@ -0,0 +1,45 @@
+#ifndef QING_FILE_READER
+#define QING_FILE_READER
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+//data's memory should be alloc first
+inline void qing_read_bin_c(const string filename, float *& data, int total_size) {
+    FILE * fin_in = fopen(filename.c_str(),  "rb");
+    if(0 == fin_in) {
+        cerr << "failed to open " << filename << endl;
+        return ;
+    }
+
+    float buf;
+    int cnt = 0;
+    while(0 != (fread(&buf, sizeof(float), 1, fin_in))) {
+        data[cnt++] = buf;
+        if(cnt >= total_size) break;
+   }
+   fclose(fin_in);
+}
+
+inline void qing_read_bin(const string filename, float *& data, int total_size) {
+    ifstream fin(filename, ios::in|ios::binary);
+    if(false == fin.is_open()) {
+        cerr << "error to open " << filename << endl;
+        return ;
+    }
+
+    fin.read((char *)(data), sizeof(float)*total_size);
+# if 0
+    for(int i = 0; i < total_size; ++i) {
+        fin.read((char *)(data + i), sizeof(float));
+    }
+# endif
+}
+
+#endif // QING_FILE_READER
+
---
+++ qing_file_writer.h
@@ -0,0 +1,32 @@
+#ifndef QING_FILE_WRITER
+#define QING_FILE_WRITER
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+//total_size: length of data
+//step_size:  length of output '\n'
+inline void qing_write_txt(const string filename, const float* data, const int total_size, const int step_size = 0) {
+    ofstream fout(filename, ios_base::out) ;
+    if(false == fout.is_open()) {
+        cerr << "failed to open " << filename << endl;
+        return ;
+    }
+
+    for(int i = 0, step_count = 0; i < total_size; ++i) {
+        fout << data[i] << ' ';
+        step_count ++;
+        if( step_size == step_count ) {
+            fout << endl;
+            step_count = 0;
+        }
+    }
+    fout.close();
+
+    cout << "end of saving " << filename << ", total_size = " << total_size << ", step_size = " << step_size << endl;
+}
+
+#endif // QING_FILE_WRITER
+
