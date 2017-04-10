e349359c4708ca7d3b78b11480f4545e79f58e17
ranqing <ranqing@didichuxing.com>
2017-04-10 12:05:30 +0800

    add more pcl including files in qing_common.h

--- qing_common.h
+++ qing_common.h
@@ -34,16 +34,34 @@
 using namespace std;
 
 //PCL header
-#include <pcl/io/pcd_io.h>
-#include <pcl/io/ply_io.h>
-#include <pcl/io/vtk_io.h>
-#include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/surface/gp3.h>
+//#include <pcl/io/pcd_io.h>
+//#include <pcl/io/ply_io.h>
+//#include <pcl/io/vtk_io.h>
+//#include <pcl/point_types.h>
+//#include <pcl/filters/voxel_grid.h>
+//#include <pcl/point_types.h>
+//#include <pcl/io/pcd_io.h>
+//#include <pcl/kdtree/kdtree_flann.h>
+//#include <pcl/features/normal_3d.h>
+//#include <pcl/surface/gp3.h>
+
+//#include <pcl/common/transforms.h>
+//#include <pcl/common/pca.h>
+//#include <pcl/filters/crop_box.h>
+//#include <pcl/visualization/point_cloud_color_handlers.h>
+//#include <pcl/visualization/pcl_visualizer.h>
+//#include <pcl/surface/convex_hull.h>
+//#include <pcl/sample_consensus/method_types.h>
+//#include <pcl/sample_consensus/model_types.h>
+//#include <pcl/segmentation/sac_segmentation.h>
+//#include <pcl/segmentation/extract_clusters.h>
+//#include <pcl/apps/dominant_plane_segmentation.h>
+//#include <pcl/visualization/image_viewer.h>
+//#include <pcl/features/moment_of_inertia_estimation.h>
+//#include <pcl/features/normal_3d.h>
+//#include <pcl/kdtree/kdtree.h>
+//#include <pcl/filters/extract_indices.h>
+//#include <pcl/filters/voxel_grid.h>
 
 //OpenCV header
 #include <opencv2/core/core.hpp>
